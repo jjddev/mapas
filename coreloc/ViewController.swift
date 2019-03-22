f1ffe8f463b5a3cf302189c4aed79064c17449bf
PUCPR <pucpr@LADMAC0114.local>
2019-03-22 20:30:10 -0300

    exemplo de mapas

--- coreloc/ViewController.swift
+++ coreloc/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let lm = CLLocationManager()
+    static let geocoder  = CLGeocoder()
+    let localInicial = CLLocation(latitude: -28.4579, longitude: -52.822)
+    
+    @IBOutlet weak var mapa: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        lm.requestWhenInUseAuthorization()
+        lm.startUpdatingLocation()
+        
+        mapa.mapType = .standard
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.0275, longitudeDelta: 0.0275)
+        let regiao = MKCoordinateRegion(center: localInicial.coordinate, span: span)
+        
+        mapa.setRegion(regiao, animated: true)
+        
+        let anotacao = MKPointAnnotation()
+        anotacao.coordinate = localInicial.coordinate
+        anotacao.title = ">>AQUI<<"
+        anotacao.subtitle = "SIM"
+        
+        mapa.addAnnotation(anotacao)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let local = locations[locations.count - 1]
+        
+        if local.horizontalAccuracy > 0 {
+            lm.stopUpdatingLocation()
+            print("\(local.coordinate.latitude) | \(local.coordinate.longitude)")
+            
+            ViewController.geocoder.reverseGeocodeLocation(local) { (placemark, _ ) in
+                if let marca = placemark?.first {
+                    print(marca.name)
+                }
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("GPS fora: \(error)")
     }
 
 
